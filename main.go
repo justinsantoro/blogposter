c5c88aa5b9b38c21e0c0f1c54a744453a97111c8
Justin Santoro <jzsantoro14@gmail.com>
2021-04-11 21:13:42 -0400

    do nothing on post push

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"context"
-	"errors"
 	"flag"
-	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"os/signal"
 	"path"
@@ -51,14 +48,6 @@ func main() {
 
 	server := NewServer(conf)
 	server.PostPush = func() error {
-		var cli = http.DefaultClient
-		resp, err := cli.Get("https://api.render.com/deploy/srv-buq9jcoti7j1rauj0th0?key=TqVqN6fk51A")
-		if err != nil {
-			return errors.New("error triggering deploy: " + err.Error())
-		}
-		if resp.StatusCode != http.StatusOK {
-			return errors.New(fmt.Sprintf("deploy hook retuned non-successful status code: %d - %s", resp.StatusCode, resp.Body))
-		}
 		return nil
 	}
 	ctx, cancel := context.WithCancel(context.Background())
